95e668d5da46e65b8d35e35630b9ecd20be32fa7
nisha-07 <nishasataki@gmail.com>
2022-12-13 17:30:51 +0530

    Router: Add package and add pages
    Use of Route, Switch, Link, navLink tags

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import './App.css';
 
+import { BrowserRouter, NavLink, Route, Switch } from 'react-router-dom';
+
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+
 function App() {
   return (
-    <div className="d-flex justify-content-center">
-      <h3>My Articles</h3>
+    <div className="d-block">
+      <BrowserRouter>
+        <nav>
+          <h3 className='col-4'>My Articles</h3>
+          <div className="col-8">
+            {/* we use Link tag instead of a because it won't send request to server everytime we click on it  */}
+            <NavLink exact to="/">Home</NavLink>
+            <NavLink to="/about">About</NavLink>
+            <NavLink to="/contact">Contact</NavLink>
+          </div>
+        </nav>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+        <div>
+            <h5>Home page</h5>
+            <p>A home page is the primary web page that a visitor will view when they navigate to a website via a search engine, and it may also function as a landing page to attract visitors..[3][4] In some cases, the home page is a site directory, particularly when a website has multiple home pages.</p>
+        </div>
+    )
+}
+
+export default Home
